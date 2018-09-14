246404ac010e214a02aeca4d4d30e5ade34a53bc
Jose Pinilla <jpinilla@ece.ubc.ca>
2018-09-13 20:19:01 -0700

    Moved drawing of tiled graph to architectures

--- embedding_methods/architectures/drawing.py
+++ embedding_methods/architectures/drawing.py
@@ -5,38 +5,61 @@ Tools to visualize graphs of architectures.
 import warnings
 import networkx as nx
 import dwave_networkx as dnx
+import matplotlib.pyplot as plt
 
+__all__ = ['draw_architecture', 'draw_architecture_embedding']
 
-__all__ = ['draw_architecture','draw_architecture_embedding']
-
-def draw_architecture(G, **kwargs):
+def draw_architecture(target_graph, **kwargs):
     """ Draws graph G according to G's family layout.
     """
-    family = G.graph['family']
+    family = target_graph.graph['family']
 
     if family == 'chimera':
-        dnx.draw_chimera(G, **kwargs)
+        dnx.draw_chimera(target_graph, **kwargs)
 
     elif family == 'pegasus':
-        dnx.draw_pegasus(G, **kwargs)
+        dnx.draw_pegasus(target_graph, **kwargs)
 
     else:
-        nx.draw_spring(G)
+        nx.draw_spring(target_graph)
         warnings.warn("Graph family not available. Using NetworkX spring layout")
 
-def draw_architecture_embedding(G,  *args, **kwargs):
+def draw_architecture_embedding(target_graph, *args, **kwargs):
     """ Draws an embedding onto the target graph G,
         according to G's family layout.
     """
-    family = G.graph['family']
+    family = target_graph.graph['family']
 
     if family == 'chimera':
-        dnx.draw_chimera_embedding(G, embedding, **kwargs)
+        dnx.draw_chimera_embedding(target_graph, *args, **kwargs)
 
     elif family == 'pegasus':
-        dnx.draw_pegasus_embedding(G, embedding, **kwargs)
+        dnx.draw_pegasus_embedding(target_graph, *args, **kwargs)
 
     else:
-        layout = nx.spring_layout(G)
-        dnx.drawing.qubit_layout.draw_embedding(G, layout,  *args, **kwargs)
+        layout = nx.spring_layout(target_graph)
+        dnx.drawing.qubit_layout.draw_embedding(target_graph, layout, *args, **kwargs)
         warnings.warn("Graph family not available. Using NetworkX spring layout")
+
+def draw_tiled_graph(m, n, tiles, topology, **kwargs):
+    """ Draws a grid representing the architecture tiles
+        with an overlay of source nodes.
+    """
+
+    concentrations = {name : "d=%s"%tile.concentration
+                            for name, tile in tiles.items() if name!=None}
+    G = nx.empty_graph()
+    G.add_nodes_from(topology.keys())
+
+    dnx.drawing.qubit_layout.draw_qubit_graph(G, topology,**kwargs)
+    fig = plt.figure(1)
+    plt.grid('on')
+    plt.axis('on')
+    plt.axis([0,n,0,m])
+    x_ticks = range(0, n) # steps are width/width = 1 without scaling
+    y_ticks = range(0, m)
+    plt.xticks(x_ticks)
+    plt.yticks(y_ticks)
+    # Label tiles
+    for (i,j), label in concentrations.items():
+        plt.text(i, j, label)
--- embedding_methods/architectures/generators.py
+++ embedding_methods/architectures/generators.py
@@ -22,56 +22,56 @@ import networkx as nx
 import dwave_networkx as dnx
 
 __all__ = ['rainier_graph', 'vesuvius_graph', 'dw2x_graph',
-            'dw2000q_graph', 'p6_graph', 'p16_graph', 'h20k_graph']
+           'dw2000q_graph', 'p6_graph', 'p16_graph', 'h20k_graph']
 
 
 def rainier_graph(**kwargs):
     """ D-Wave One 'Rainier' Quantum Annealer graph
         https://en.wikipedia.org/wiki/D-Wave_Systems
     """
-    G = dnx.generators.chimera_graph(4, 4, 4, **kwargs)
-    G.name = 'Rainier'
-    return G
+    target_graph = dnx.generators.chimera_graph(4, 4, 4, **kwargs)
+    target_graph.name = 'Rainier'
+    return target_graph
 
 def vesuvius_graph(**kwargs):
     """ D-Wave Two 'Vesuvius' Quantum Annealer graph
         https://en.wikipedia.org/wiki/D-Wave_Systems
     """
-    G = dnx.generators.chimera_graph(8, 8, 4, **kwargs)
-    G.name = 'Vesuvius'
-    return G
+    target_graph = dnx.generators.chimera_graph(8, 8, 4, **kwargs)
+    target_graph.name = 'Vesuvius'
+    return target_graph
 
 def dw2x_graph(**kwargs):
     """ D-Wave 2X Quantum Annealer graph
         https://en.wikipedia.org/wiki/D-Wave_Systems
     """
-    G = dnx.generators.chimera_graph(12, 12, 4, **kwargs)
-    G.name = 'DW2X'
-    return G
+    target_graph = dnx.generators.chimera_graph(12, 12, 4, **kwargs)
+    target_graph.name = 'DW2X'
+    return target_graph
 
 def dw2000q_graph(**kwargs):
     """ D-Wave 2000Q Quantum Annealer graph
         https://en.wikipedia.org/wiki/D-Wave_Systems
     """
-    G = dnx.generators.chimera_graph(16, 16, 4, **kwargs)
-    G.name = 'DW2000Q'
-    return G
+    target_graph = dnx.generators.chimera_graph(16, 16, 4, **kwargs)
+    target_graph.name = 'DW2000Q'
+    return target_graph
 
 def p6_graph(**kwargs):
     """ Pegasus 6 graph
         https://www.dwavesys.com/sites/default/files/mwj_dwave_qubits2018.pdf
     """
-    G = dnx.generators.pegasus_graph(6, **kwargs)
-    G.name = 'P6'
-    return G
+    target_graph = dnx.generators.pegasus_graph(6, **kwargs)
+    target_graph.name = 'P6'
+    return target_graph
 
 def p16_graph(**kwargs):
     """ Pegasus 16 graph
         https://www.dwavesys.com/sites/default/files/mwj_dwave_qubits2018.pdf
     """
-    G = dnx.generators.pegasus_graph(16, **kwargs)
-    G.name = 'P16'
-    return G
+    target_graph = dnx.generators.pegasus_graph(16, **kwargs)
+    target_graph.name = 'P16'
+    return target_graph
 
 def h20k_graph(data=True, coordinates=False):
     """ HITACHI 20k-Spin CMOS digital annealer graph.
@@ -79,26 +79,27 @@ def h20k_graph(data=True, coordinates=False):
     """
     n, m, t = 128, 80, 2
 
-    G = nx.grid_graph(dim=[t,m,n])
+    target_graph = nx.grid_graph(dim=[t, m, n])
 
-    G.name = 'HITACHI 20k'
+    target_graph.name = 'HITACHI 20k'
     construction = (("family", "hitachi"),
                     ("rows", 5), ("columns", 4),
                     ("data", data),
                     ("labels", "coordinate" if coordinates else "int"))
 
-    G.graph.update(construction)
+    target_graph.graph.update(construction)
 
     if coordinates:
         if data:
-            for q in G:
-                (z, y, x) = q
-                G.node[q]['linear_index'] = x + n*(y + m*z)
+            for t_node in target_graph:
+                (z_coord, y_coord, x_coord) = t_node
+                linear = x_coord + n*(y_coord + m*z_coord)
+                target_graph.node[t_node]['linear_index'] = linear
     else:
-        coordinate_labels = { (x,y,z) : x + n*(y + m*z) for (x, y, z) in G }
+        coordinate_labels = {(x, y, z):x+n*(y+m*z) for (x, y, z) in target_graph}
         if data:
-            for q in G:
-                G.node[q]['grid_index'] = q
-        G = nx.relabel_nodes(G, coordinate_labels)
+            for t_node in target_graph:
+                target_graph.node[t_node]['grid_index'] = t_node
+        target_graph = nx.relabel_nodes(target_graph, coordinate_labels)
 
-    return G
+    return target_graph
