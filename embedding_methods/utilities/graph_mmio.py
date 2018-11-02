f98e9b23ac38088ddd62ed919f69f0cd95ace58e
Jose Pinilla <jpinilla@ece.ubc.ca>
2018-11-02 16:51:21 -0700

    Corrected position labels being stored in MTX file

--- embedding_methods/utilities/graph_mmio.py
+++ embedding_methods/utilities/graph_mmio.py
@@ -35,31 +35,31 @@ def read(mtx_name, mm_dir=MM_DIR, data=True):
         in the output.
 
     Return:
-        G (ndarray): array recovered from given file with or w/o auxiliary data.
+        mtx (ndarray): array recovered from given file with or w/o auxiliary data.
 
     """
     base, prefix, seq = mtx_name.partition('_G')
 
     mtx_filepath = mm_dir + mtx_name + MM_EXT
-    G = mmread(mtx_filepath)
+    mtx = mmread(mtx_filepath)
 
     if data:
         Gname = base + prefix + 'name' + seq
         name_filepath = mm_dir + Gname + TXT_EXT
         name =  open(name_filepath) if isfile(name_filepath) else mtx_name
-        G.name =  name
+        mtx.name =  name
 
         Gcoord = base + prefix + 'coord' + seq
         coord_filepath = mm_dir + Gcoord + MM_EXT
         coord = mmread(coord_filepath) if isfile(coord_filepath) else None
-        G.coord =  coord
+        mtx.coord =  coord
 
         Gnodename = base + prefix + 'nodename' + seq
-        nodename_filepath = mm_dir + Gnodename + TXT_EXT
+        nodename_filepath = mm_dir + Gnodename + MM_EXT
         nodename = mmread(nodename_filepath) if isfile(nodename_filepath) else []
-        G.nodename =  nodename
+        mtx.nodename =  nodename
 
-    return G
+    return mtx
 
 def read_networkx(mtx_name, mm_dir=MM_DIR, data=True):
     """ Read from a Matrix Market file and return a NetworkX Graph
@@ -76,22 +76,23 @@ def read_networkx(mtx_name, mm_dir=MM_DIR, data=True):
         Gnx (Graph): NetworkX Graph recovered from given file with
         or w/o auxiliary data.
     """
-    G = read(mtx_name, mm_dir, data)
-    Gnx = nx.Graph(G)
+    mtx = read(mtx_name, mm_dir, data)
+    Gnx = nx.Graph(mtx)
 
     if data:
-        Gnx.name = G.name
-        labels = { i:label for i, label in enumerate(G.nodename, start=1) }
-        nx.relabel_nodes(Gnx, labels, copy=False)
+        Gnx.name = mtx.name
+
+        labels = { i:int(label) for i, label in enumerate(mtx.nodename) }
+        if mtx.nodename: nx.relabel_nodes(Gnx, labels, copy=False)
         pos = {}
-        if G.coord is not None:
-            _, dim = G.coord.shape
-            for i, v in enumerate(Gnx):
+        if mtx.coord is not None:
+            _, dim = mtx.coord.shape
+            for i, v in labels.items():
                 if dim==3:
-                    x,y,z = G.coord[i]
+                    x,y,z = mtx.coord[i]
                     pos[v] = x+z, y+z
                 elif dim==2:
-                    x,y = G.coord[i]
+                    x,y = mtx.coord[i]
                     pos[v] = x, y
                 else:
                     raise ValueError("Invalid XY or XYZ coordinate dimensions")
@@ -103,6 +104,7 @@ def read_mapping(mtx_a_name, mtx_b_name, mm_dir=MM_DIR, data=True):
     """
 
     """
+    #TODO: Read out embedding from Matrix Market files
     embedding = {}
     return embedding
 
@@ -128,8 +130,8 @@ def write_networkx(Gnx, pos=None, mtx_name=None, mm_dir=MM_DIR, data=True, **par
 
     mtx_filepath = mm_dir + mtx_name + MM_EXT
 
-    G = coo_matrix(nx.to_numpy_matrix(Gnx))
-    mmwrite(mtx_filepath, G, **params)
+    mtx = coo_matrix(nx.to_numpy_matrix(Gnx))
+    mmwrite(mtx_filepath, mtx, **params)
 
     if data:
         base, prefix, seq = mtx_name.partition('_G')
@@ -147,7 +149,7 @@ def write_networkx(Gnx, pos=None, mtx_name=None, mm_dir=MM_DIR, data=True, **par
 
         Gnodename = base + prefix + 'nodename' + seq
         nodename_filepath = mm_dir + Gnodename + MM_EXT
-        Gnx_ints = np.array(nx.convert_node_labels_to_integers(Gnx).nodes())
+        Gnx_ints = np.array(Gnx.nodes())
         nodename = np.ndarray(  shape=(len(Gnx),1),
                                 dtype=int,
                                 buffer=Gnx_ints)
